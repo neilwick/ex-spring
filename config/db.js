66bc631f13f862b4c60717466f08eeb79456a1a3
Neil Wick <neil@wick.net>
2021-03-10 09:14:49 -0500

    separated secrets from db.js

---
+++ config/db.js
@@ -0,0 +1,24 @@
+
+const mariadb = require('mariadb');
+const secrets = require('./secrets');
+
+module.exports = {
+    connected: false,
+    init: function () {
+        if (!this.connected) {
+            try {
+                this.pool = mariadb.createPool(secrets.database);
+                this.connected = true;
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    },
+    getConnection: async function () {
+        if (this.connected) {
+            return await this.pool.getConnection();
+        } else {
+            console.log("Database not connected!");
+        }
+    }
+}
